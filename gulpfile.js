1c75bc88a6d3501c03fae4ea0a779d3b327098da
SneakBug8 <sneakbug8@gmail.com>
2019-07-23 13:34:38 +0300

    gulp for sass

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function ()
+{
+    return gulp.src('assets/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('/'));
+});
+
+gulp.task('sass:watch', function ()
+{
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+export.build = gulp.series("sass");
